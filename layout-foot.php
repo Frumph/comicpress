ef3f21a73c9202e0dea02ee50a7689ad2a7a5df6
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2011-01-27 01:09:33 -0800

    removing a couple of the extra .png's and some css changes to the style.css, todo:  finish all of the layouts making sure they're set right
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- layout-foot.php
+++ layout-foot.php
@@ -1,12 +1,13 @@
 		</div>
 		<?php if (is_cp_theme_layout('lgn,rgn')) { ?>
 			<?php if (is_cp_theme_layout('lgn') && !comicpress_disable_sidebars()) get_sidebar('right');  ?>
-			<div class="clear"></div>
-		</div>
+				<div class="clear"></div>
+			</div>
 		<?php } ?>
 		<?php if (is_cp_theme_layout('3c2r,v3cr') && !comicpress_disable_sidebars()) get_sidebar('left'); ?>
 		<?php if (is_cp_theme_layout('2cr,2cvr,3c,3c2r,v3c,v3cr,rgn') && !comicpress_disable_sidebars()) get_sidebar('right');  ?>
-		<div class="clear"></div>
+			<div class="clear"></div>
+		</div>
 		<div id="subcontent-wrapper-foot"></div>
 	</div>
 	<div class="clear"></div>
