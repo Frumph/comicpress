9b5c6fc02699829f7e2671d86b2d5e661fb05ff6
Frumph <philip@frumph.net>
2012-12-17 08:54:26 -0800

    removing the function_exists check, since there are filters on each of the functions, .. don't need both

--- functions/syndication.php
+++ functions/syndication.php
@@ -23,28 +23,24 @@ if (comicpress_themeinfo('enable_comment_count_in_rss')) {
 }
 
 //Insert the comic image into the RSS feed
-if (!function_exists('comicpress_comic_feed')) {
-	function comicpress_comic_feed($content = '') { 
-		global $wp_query, $post, $comiccat;
-		if ($wp_query->query_vars['cat'] == $comiccat ) {
-			$content .= '<p><a href="'.get_permalink().'" title="'.comicpress_the_hovertext($post).'">'.comicpress_display_comic_thumbnail('comic',$post,true).'</a></p>';
-		} else {
-			$content .= '<p><a href="'.get_permalink().'" title="'.comicpress_the_hovertext($post).'">'.comicpress_display_comic_thumbnail('rss',$post,true).'</a></p>';
-		}
-		return apply_filters('comicpress_comic_feed', $content);
+function comicpress_comic_feed($content = '') { 
+	global $wp_query, $post, $comiccat;
+	if ($wp_query->query_vars['cat'] == $comiccat ) {
+		$content .= '<p><a href="'.get_permalink().'" title="'.comicpress_the_hovertext($post).'">'.comicpress_display_comic_thumbnail('comic',$post,true).'</a></p>';
+	} else {
+		$content .= '<p><a href="'.get_permalink().'" title="'.comicpress_the_hovertext($post).'">'.comicpress_display_comic_thumbnail('rss',$post,true).'</a></p>';
 	}
+	return apply_filters('comicpress_comic_feed', $content);
 }
 
 // removed the comicpress_in_comic_category so that if it has a post-image it will add it to the rss feed (else rss comic thumb)
-if (!function_exists('comicpress_insert_comic_feed')) {
-	function comicpress_insert_comic_feed($content = '') {
-		global $post;
-		$category = get_the_category($post->ID);
-		if (comicpress_in_comic_category($category[0]->cat_ID)) {
-			$content = comicpress_comic_feed().$content;
-		}
-		return apply_filters('comicpress_insert_comic_feed', $content);
+function comicpress_insert_comic_feed($content = '') {
+	global $post;
+	$category = get_the_category($post->ID);
+	if (comicpress_in_comic_category($category[0]->cat_ID)) {
+		$content = comicpress_comic_feed().$content;
 	}
+	return apply_filters('comicpress_insert_comic_feed', $content);
 }
 
 add_filter('the_content_feed','comicpress_insert_comic_feed');
