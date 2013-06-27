2398d6a96e6fa9481ba69a2ae58750428823ed53
Philip M. Hofer (Frumph) <philip@frumph.net>
2013-06-26 17:24:57 -0700

    remove offending call to ro.gif that is being reported as malicious

--- options/layout.php
+++ options/layout.php
@@ -16,7 +16,6 @@
 		<br />
 		<?php _e('Developed and maintained by','comicpress'); ?> <a href="http://frumph.net/">Philip M. Hofer</a> <small>(<a href="http://frumph.net/">Frumph</a>)</small>, <?php _e('Originally created by','comicpress'); ?> <a href="http://mindfaucet.com/">Tyler Martin</a><br />
 		<?php _e('If you like the ComicPress theme, please donate.  It will help in developing new features and versions.','comicpress'); ?><br />
-		Romanian <img src="http://mobirout.com/uploads/ro.gif" alt="Romanian translation" /> translation, Provided by: Web Geek Science (webhostinggeeks.com)
 		<table style="margin:0 auto;">
 			<tr>
 				<td style="width:200px;">
