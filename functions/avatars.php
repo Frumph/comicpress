9cf6be8a412cd9570fc50d3937e8a14779d2c33c
Frumph <philip@frumph.net>
2014-01-02 19:58:00 -0800

    So the function can be overridden if someone wants to use jetpacks' facebook stuff

--- functions/avatars.php
+++ functions/avatars.php
@@ -1,32 +1,36 @@
 <?php
 
-function comicpress_random_default_avatar($id_or_email = '') {
-	$current_avatar_directory = comicpress_themeinfo('avatar_directory');
-	
-	if (!empty($current_avatar_directory) && ($current_avatar_directory !== 'none')) {
-		if (is_dir(comicpress_themeinfo('stylepath') . '/images/avatars/' . $current_avatar_directory)) {
-			$count = count($results = glob(comicpress_themeinfo('stylepath') . '/images/avatars/'.$current_avatar_directory.'/*'));
-			$blogurl = comicpress_themeinfo('styleurl');
-		} else {
-			$count = count($results = glob(comicpress_themeinfo('themepath') . '/images/avatars/'.$current_avatar_directory.'/*'));
-			$blogurl = comicpress_themeinfo('themeurl');
-		}		
-		if ($count) { 
-			$default = '';
-			
-			$checknum = hexdec(substr(md5($id_or_email),0,5)) % $count;
-			if ($count > 0) {
-				$default = basename($results[(int)$checknum]); 
+if (!function_exists('comicpress_random_default_avatar')) {
+	function comicpress_random_default_avatar($id_or_email = '') {
+		$current_avatar_directory = comicpress_themeinfo('avatar_directory');
+		
+		if (!empty($current_avatar_directory) && ($current_avatar_directory !== 'none')) {
+			if (is_dir(comicpress_themeinfo('stylepath') . '/images/avatars/' . $current_avatar_directory)) {
+				$count = count($results = glob(comicpress_themeinfo('stylepath') . '/images/avatars/'.$current_avatar_directory.'/*'));
+				$blogurl = comicpress_themeinfo('styleurl');
 			} else {
-				return false;
-			}
-			return $blogurl.'/images/avatars/'.$current_avatar_directory.'/'.$default;
-		} else
+				$count = count($results = glob(comicpress_themeinfo('themepath') . '/images/avatars/'.$current_avatar_directory.'/*'));
+				$blogurl = comicpress_themeinfo('themeurl');
+			}		
+			if ($count) { 
+				$default = '';
+				
+				$checknum = hexdec(substr(md5($id_or_email),0,5)) % $count;
+				if ($count > 0) {
+					$default = basename($results[(int)$checknum]); 
+				} else {
+					return false;
+				}
+				return $blogurl.'/images/avatars/'.$current_avatar_directory.'/'.$default;
+			} else
+				return get_option('avatar_default');
+		} else {
 			return get_option('avatar_default');
-	} else {
-		return get_option('avatar_default');
+		}
+		return false;
 	}
-	return false;
 }
 
-?>
+function temp_random_default_avatar($id_or_email = '') {
+	return get_option('avatar_default');
+}
