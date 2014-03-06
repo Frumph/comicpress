d403eec51ff07e736b01b8e318665bc87bdb4e11
Frumph <philip@frumph.net>
2014-03-06 05:41:27 -0800

    sidebar.php cleanup; so it shows empty if it executes from a plugin

--- sidebar.php
+++ sidebar.php
@@ -2,15 +2,7 @@
 	<div class="sidebar">
 	<?php 
 		do_action('sidebar');
-		if ( !dynamic_sidebar() ) { ?>
-		<div class="sidebar-no-widgets">
-			<?php _e('There are currently no widgets assigned to the left-sidebar, place some!','comicpress'); ?><br />
-			<br />
-			<?php _e('Once you add widgets to this sidebar, this default information will go away.','comicpress'); ?><br />
-			<br />
-			<?php _e('Widgets can be added by going to your dashboard (wp-admin) -> Appearance -> Widgets, drag a widget you want to see into one of the appropriate sidebars.','comicpress'); ?><br />
-		</div>
-		<?php }
+		dynamic_sidebar();
 	?>
 	</div>
 </div>
