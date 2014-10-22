3036cff9712a07b5c05ed20113ac4499dcfd832d
Frumph <philip@frumph.net>
2014-10-22 08:24:59 -0700

    Make use of the checkbox for displaying the header text in the appearance -> header Add meta code for better mobile image support

--- functions/custom-header.php
+++ functions/custom-header.php
@@ -1,6 +1,7 @@
 <?php
 
 $custom_header_args = array(
+			'default-text-color' => 'B00',
 			'flex-width' => true,
 			'width' => comicpress_themeinfo('custom_image_header_width'),
 			'flex-height' => true,
@@ -15,20 +16,27 @@ add_theme_support( 'custom-header', $custom_header_args );
 function comicpress_admin_header_style() { ?>
 <style type="text/css">
 	#headimg { width: <?php echo get_custom_header()->width; ?>px; height: <?php echo get_custom_header()->height; ?>px; background: url(<?php header_image(); ?>) top center no-repeat; }
+<?php if (!display_header_text()) { ?>	
 	#headimg h1, #headimg .description { display: none; }
+<?php } ?>
 </style>
 	<?php
 }
 
 function comicpress_header_style() { 
-	if (get_header_image()) { ?>
+	if (get_header_image()) { 
+		$textcolor = get_header_textcolor()	
+?>
 <style type="text/css">
 	#header { width: <?php echo get_custom_header()->width; ?>px; height: <?php echo get_custom_header()->height; ?>px; background: url('<?php header_image(); ?>') top center no-repeat; overflow: hidden; }
 <?php if (get_theme_mod('comicpress-customize-checkbox-header-hotspot', false)) { ?>
 	#header h1 { padding: 0; }
 	#header h1 a { display: block; width: <?php echo get_custom_header()->width; ?>px; height: <?php echo get_custom_header()->height; ?>px; text-indent: -9999px; }
-	#header .description { display: none; }	
 	.header-info, .header-info h1 a { padding: 0; }
+<?php } elseif (!display_header_text()) { ?>
+	#header h1, #header .description { display: none; }
+<?php } elseif (!empty($textcolor)) { ?>
+	#header h1, #header h1 a { color: #<?php echo $textcolor; ?>; }
 <?php } ?>
 </style>
 	<?php }
