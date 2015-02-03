ed3d32ebfae6113d96ee2d1e4589e302404369e3
Kniebremser <kniebremser@fluchtsportler.de>
2015-02-03 22:47:23 +0100

    Fixed sidebars
    delete Recommended Plugins

--- sidebar-left.php
+++ sidebar-left.php
@@ -1,16 +1,18 @@
 <div id="sidebar-left">
-	<div class="sidebar">
-	<?php 
-		do_action('comicpress-sidebar-left');
-		if ( !dynamic_sidebar('sidebar-left-sidebar') ) { ?>
-		<div class="sidebar-no-widgets">
-			<?php _e('There are currently no widgets assigned to the left-sidebar, place some!','comicpress'); ?><br />
-			<br />
-			<?php _e('Once you add widgets to this sidebar, this default information will go away.','comicpress'); ?><br />
-			<br />
-			<?php _e('Widgets can be added by going to your dashboard (wp-admin) &#10132; Appearance &#10132; Widgets, drag a widget you want to see into one of the appropriate sidebars.','comicpress'); ?><br />
-		</div>
-		<?php }
-	?>
-	</div>
+    <div class="sidebar">
+    <?php 
+        do_action('easel-sidebar-left');
+        if ( !dynamic_sidebar('sidebar-left-sidebar') ) { ?>
+        <div class="sidebar-no-widgets">
+            <?php _e('There are currently no widgets assigned to the left-sidebar, place some!', 'comicpress'); ?>
+            <br />
+            <br />
+            <?php _e('Once you add widgets to this sidebar, this default information will go away.', 'comicpress'); ?>
+            <br />
+            <br />
+            <?php _e('Widgets can be added by going to your dashboard (wp-admin) &#10132; Appearance &#10132; Widgets, drag a widget you want to see into one of the appropriate sidebars.', 'comicpress'); ?>
+            <br />
+        </div>
+        <?php }  ?>
+    </div>
 </div>
--- sidebar-right.php
+++ sidebar-right.php
@@ -1,20 +1,18 @@
 <div id="sidebar-right">
-	<div class="sidebar">
-	<?php
+    <div class="sidebar">
+        <?php
 		do_action('comicpress-sidebar-right');
 		if ( !dynamic_sidebar('sidebar-right-sidebar') ) { ?>
-		<div class="sidebar-no-widgets">
-			<?php _e('There are currently no widgets assigned to the right-sidebar, place some!', 'comicpress'); ?><br />
-			<br />
-			<?php _e('Once you add widgets to this sidebar, this default information will go away.', 'comicpress'); ?><br />
-			<br />
-			<?php _e('This theme also uses the WordPress 3.0 Menu system. You probably see the default stuff you have in the menubar. Go to Appearance &#10132; Menu in the wp-admin (dashboard) and create a new menu.', 'comicpress'); ?><br />
-		</div>
-			<h2><?php _e('Recommended Plugins', 'comicpress'); ?></h2>
-			<ol>
-				<li><a href="https://wordpress.org/plugins/comic-easel/">Comic Easel</a></li>
-				<li><a href="https://wordpress.org/plugins/vipers-video-quicktags/">Viper's Video Quicktags</a></li>
-			</ol>
-		<?php } ?>
-	</div>
+        <div class="sidebar-no-widgets">
+            <?php _e('There are currently no widgets assigned to the right-sidebar, place some!', 'comicpress'); ?>
+            <br />
+            <br />
+            <?php _e('Once you add widgets to this sidebar, this default information will go away.', 'comicpress'); ?>
+            <br />
+            <br />
+            <?php _e('Widgets can be added by going to your dashboard (wp-admin) &#10132; Appearance &#10132; Widgets, drag a widget you want to see into one of the appropriate sidebars.', 'comicpress'); ?>
+            <br />
+        </div>
+        <?php } ?>
+    </div>
 </div>
