68da8f42ebce729b64e6a358815b7c3462265479
Kniebremser <kniebremser@fluchtsportler.de>
2015-03-09 11:00:24 +0100

    fixed errors in scroll.js
    + add missing "use strict" syntax for functions
    + fixed formatting errors

--- js/scroll.js
+++ js/scroll.js
@@ -3,26 +3,29 @@
 var up_timer;
 
 function getPosition() {
+    "use strict";
     yoko = document.body.scrollLeft || document.documentElement.scrollLeft;
     tate = document.body.scrollTop  || document.documentElement.scrollTop;
 
 }
 
 function pageup(x, y) {
-    if (up_timer) clearTimeout(up_timer);
-	if(y >= 1){
-		getPosition();
-		var divisionY = (tate-(tate/5));
-		var Y = Math.floor(divisionY);
-		window.scrollTo(yoko,Y);
-		up_timer = setTimeout("pageup("+yoko+","+Y+")",2);
-	}else{
-		window.scrollTo(yoko,0);
-		clearTimeout(up_timer);
-	}
-}
+    "use strict";
+    if (up_timer) {clearTimeout(up_timer);
+        if (y >= 1) {
+            getPosition();
+            var divisionY = (tate - (tate / 5));
+            var Y = Math.floor(divisionY);
+            window.scrollTo(yoko, Y);
+            up_timer = setTimeout("pageup(" + yoko + "," + Y + ")", 2);
+        } else {
+            window.scrollTo(yoko, 0);
+            clearTimeout(up_timer);
+        } 
+            }
 
-function scrollup(){
-		getPosition();
-		pageup(yoko,tate);
+    function scrollup() {
+        getPosition();
+        pageup(yoko, tate);
+    }
 }
