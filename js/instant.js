3b8c934f6b9216ee3666320d03f6bdeacc002677
Kniebremser <kniebremser@fluchtsportler.de>
2015-03-09 13:13:28 +0100

    instant.js update to 2.4
    with subsequent errors are corrected according jshint and formatting
    errors

--- js/instant.js
+++ js/instant.js
@@ -1,5 +1,5 @@
 /**
- * instant.js 2.21 (18-Mar-2009)
+ * instant.js 2.4 (10-Aug-2010)
  * (c) by Christian Effenberger 
  * All Rights Reserved
  * Source: instant.netzgesta.de
@@ -10,13 +10,13 @@
  * private web sites only 
 **/
 
-var tmp = navigator.appName == 'Microsoft Internet Explorer' && navigator.userAgent.indexOf('Opera') < 1 ? 1 : 0;
-if(tmp) var isIE = document.namespaces ? 1 : 0;
+var tmp = navigator.appName === 'Microsoft Internet Explorer' && navigator.userAgent.indexOf('Opera') < 1 ? 1 : 0;
+if (tmp) var isIE = document.namespaces && (!document.documentMode || document.documentMode < 9) ? 1 : 0;
 
-if(isIE) {
-	if(document.namespaces['v']==null) {
-		var e=["shape","shapetype","group","background","path","formulas","handles","fill","stroke","shadow","textbox","textpath","imagedata","line","polyline","curve","roundrect","oval","rect","arc","image"],s=document.createStyleSheet(); 
-		for(var i=0; i<e.length; i++) {s.addRule("v\\:"+e[i],"behavior: url(#default#VML);");} document.namespaces.add("v","urn:schemas-microsoft-com:vml");
+    if (isIE) {
+        if (document.namespaces['v'] === null) {
+            var e = ["shape", "shapetype", "group", "background", "path", "formulas", "handles", "fill", "stroke", "shadow", "textbox", "textpath", "imagedata", "line", "polyline", "curve", "roundrect", "oval", "rect", "arc", "image"], s = document.createStyleSheet();
+            for (var i=0; i<e.length; i++) {s.addRule("v\\:"+e[i],"behavior: url(#default#VML);");} document.namespaces.add("v","urn:schemas-microsoft-com:vml");
 	} 
 }
 
@@ -41,7 +41,7 @@ function getClasses(classes,string){
 	for (var j=0;j<classes.length;j++) {
 		if (classes[j] != string) {
 			if (temp) {
-				temp += ' '
+				temp += ' ';
 			}
 			temp += classes[j];
 		}
@@ -51,7 +51,7 @@ function getClasses(classes,string){
 function getClassValue(classes,string){
 	var temp = 0; var pos = string.length;
 	for (var j=0;j<classes.length;j++) {
-		if (classes[j].indexOf(string) == 0) {
+		if (classes[j].indexOf(string) === 0) {
 			temp = Math.min(classes[j].substring(pos),100);
 			break;
 		}
@@ -61,7 +61,7 @@ function getClassValue(classes,string){
 function getClassColor(classes,string){
 	var temp = 0; var str = ''; var pos = string.length;
 	for (var j=0;j<classes.length;j++) {
-		if (classes[j].indexOf(string) == 0) {
+		if (classes[j].indexOf(string) === 0) {
 			temp = classes[j].substring(pos);
 			str = '#' + temp.toLowerCase();
 			break;
@@ -76,7 +76,7 @@ function getClassColor(classes,string){
 function getClassAttribute(classes,string){
 	var temp = 0; var pos = string.length;
 	for (var j=0;j<classes.length;j++) {
-		if (classes[j].indexOf(string) == 0) {
+		if (classes[j].indexOf(string) === 0) {
 			temp = 1; 
 			break;
 		}
@@ -94,7 +94,7 @@ function addShading(ctx,x,y,width,height,opacity) {
 }
 function addLining(ctx,x,y,width,height,opacity,inset,inner,color) {
 	var style = ctx.createLinearGradient(x,y,width,height);
-	if(inner==true) {
+	if(inner===true) {
 		style.addColorStop(0,'rgba(192,192,192,'+opacity+')');
 		style.addColorStop(0.7,'rgba(254,254,254,0.8)');
 		style.addColorStop(1,'rgba(254,254,254,0.9)');
@@ -158,7 +158,7 @@ function getRadius(radius,width,height){
 	return radius+'%';
 }
 function wavedRect(ctx,x,y,w,h,r,n){
-	function rF(a,z) {return Math.random()*(z-a)+a;};
+	function rF(a,z) {return Math.random()*(z-a)+a;}
 	var i,t,c,cx,cy,cw,ch,wa=w/16,wz=w/32,ha=h/16,hz=h/32,da=r*0.1,dz=r*0.25; if(!n) {ctx.beginPath();} ctx.moveTo(x,y);
 	cx=x; cy=y; ch=h; while(ch>0) {t=rF(ha,Math.min(ch,hz)); c=rF(1,t); ctx.quadraticCurveTo(cx+rF(da,dz),cy+c,cx,cy+t); cy+=t; ch-=t;}
 	cx=x; cy=y+h; cw=w; while(cw>0) {t=rF(wa,Math.min(cw,wz)); c=rF(1,t); ctx.quadraticCurveTo(cx+c,cy-rF(da,dz),cx+t,cy); cx+=t; cw-=t;}
@@ -167,7 +167,7 @@ function wavedRect(ctx,x,y,w,h,r,n){
 	if(!n) ctx.closePath();
 }
 function wavedPath(x,y,w,h,r){
-	function rI(a,b) {return parseInt(Math.floor(Math.random()*(b-a+1))+a);};
+	function rI(a,b) {return parseInt(Math.floor(Math.random()*(b-a+1))+a);}
 	function qC(cX,cY,CPx,CPy,aX,aY) {var z=new Array(6); z[0]=cX+2.0/3.0*(CPx-cX); z[1]=cY+2.0/3.0*(CPy-cY); z[2]=z[0]+(aX-cX)/3.0; z[3]=z[1]+(aY-cY)/3.0; z[4]=aX; z[5]=aY; return z;}
 	var p="",i,k,t,c,cx,cy,cw,ch,wa=w/16,wz=w/32,ha=h/16,hz=h/32,da=r*0.1,dz=r*0.25; 
 	p+='m '+x+','+y; cx=x; cy=y; ch=h; while(ch>0) {t=rI(ha,Math.min(ch,hz)); c=rI(1,t); k=qC(cx,cy,cx+rI(da,dz),cy+c,cx,cy+t); 
@@ -215,9 +215,9 @@ function addIEInstant() {
 			ishadow = getClassValue(classes,"ishadow");
 			if(ishadow>0) opacity=ishadow/100;
 			icolor = getClassColor(classes,"icolor");
-			if(icolor!=0) color = icolor;
+			if(icolor!==0) color = icolor;
 			itxtcol = getClassColor(classes,"itxtcol");
-			if(itxtcol!=0) tcolor = itxtcol;
+			if(itxtcol!==0) tcolor = itxtcol;
 			itxttitle = getClassAttribute(classes,"itxttitle");
 			itxtalt = getClassAttribute(classes,"itxtalt");
 			itiltleft = getClassAttribute(classes,"itiltleft");
@@ -227,16 +227,16 @@ function addIEInstant() {
 			noshading = getClassAttribute(classes,"noshading");
 			nocorner = getClassAttribute(classes,"nocorner");
 			preserve = getClassAttribute(classes,"preserve");
-			if(historical==true) nocorner = false;
-			if(itiltright==true) tilt = 'r';
-			if(itiltnone==true) tilt = 'n';
-			if(itiltleft==true) tilt = 'l';
+			if(historical===true) nocorner = false;
+			if(itiltright===true) tilt = 'r';
+			if(itiltnone===true) tilt = 'n';
+			if(itiltleft===true) tilt = 'l';
 			newClasses = getClasses(classes,"instant");
 			width = image.width; height = image.height;
 			border = Math.round(((width+height)/2)*0.05); db=border;
 			offset = border/2; inset = parseInt(offset*.75);
 			ww=width-(border*2); hh=height-(border*2); hz=Math.round(hh/3);
-			f=(noshading==0?"t":"f"); r=nocorner?getRadius(border,width,height):0;
+			f=(noshading===0?"t":"f"); r=nocorner?getRadius(border,width,height):0;
 			if(tilt=='r') {
 				rotation = 2.8; scale = 0.95; tilt = 'n';
 			}else if(tilt=='n') {
@@ -248,24 +248,24 @@ function addIEInstant() {
 			vml = document.createElement(['<var style="zoom:1;overflow:hidden;display:' + display + ';width:' + width + 'px;height:' + height + 'px;padding:0px;">'].join(''));
 			flt = image.currentStyle.styleFloat.toLowerCase();
 			display = (flt=='left'||flt=='right')?'inline':display;
-			text = image.alt!=''&&itxtalt!=0?image.alt:image.title!=''&&itxttitle!=0?image.title:'';
+			text = image.alt!==''&&itxtalt!==0?image.alt:image.title!==''&&itxttitle!==0?image.title:'';
 			head = '<v:group style="rotation:' + rotation + '; zoom:' + scale + '; display:' + display + '; margin:-1px 0 0 -1px; padding:0px; position:relative; width:'+width+'px;height:'+height+'px;" coordsize="'+width+','+height+'"><v:rect strokeweight="0" filled="f" stroked="f" fillcolor="transparent" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:0px;left:0px;width:'+width+'px;height:'+height+'px;"><v:fill opacity="0" color="#000000" /></v:rect>';
 			shadow = '<v:roundrect arcsize="'+r+'" strokeweight="0" filled="t" stroked="f" fillcolor="#000000" style="filter:progid:dxImageTransform.Microsoft.Blur(PixelRadius='+inset+', MakeShadow=false) progid:dxImageTransform.Microsoft.Alpha(opacity='+(opacity*100)+'); zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:'+offset+'px;left:'+offset+'px;width:'+(width-(2*offset))+'px;height:'+(height-(2*offset))+'px;"><v:fill color="#000000" opacity="1" /></v:roundrect>';
-			if(historical==0) {
+			if(historical===0) {
 				frame = '<v:roundrect arcsize="'+r+'" strokeweight="0" filled="t" stroked="f" fillcolor="'+color+'" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:0px;left:0px;width:'+(width-offset)+'px;height:'+(height-offset)+'px;"></v:roundrect>';
 			}else {path = wavedPath(0,0,(width-offset)*10,(height-offset)*10,border*10);
 				frame = '<v:shape strokeweight="0" stroked="f" filled="t" fillcolor="'+color+'" coordorigin="0,0" coordsize="'+((width-offset)*10)+','+((height-offset)*10)+'" path="'+path+'" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:0px;left:0px;width:'+(width-offset)+'px;height:'+(height-offset)+'px;"></v:shape>';
 			}
 			shine = '<v:rect strokeweight="0" filled="t" stroked="f" fillcolor="' + color + '" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:'+db+'px;left:'+db+'px;width:' + (width-offset-(2*border)) + 'px;height:' + (height-offset-(2*border)) + 'px;"><v:fill color="#000000" opacity="' + opacity + '" /></v:rect>';
-			if(typeof check_strokeTextCapability=='function' && check_strokeTextCapability() && text!='') {				
-				over = '<v:rect strokeweight="0" filled="t" stroked="f" fillcolor="'+color+'" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;left:'+(border-1)+'px;top:'+(height-1-offset-(border*(document.documentMode==8&&rotation!=0?4:3)))+'px;width:'+(width-offset+2-(border*2))+'px;height:'+(border*3)+'px;"></v:rect>';
+			if(typeof check_strokeTextCapability=='function' && check_strokeTextCapability() && text!=='') {				
+				over = '<v:rect strokeweight="0" filled="t" stroked="f" fillcolor="'+color+'" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;left:'+(border-1)+'px;top:'+(height-1-offset-(border*(document.documentMode==8&&rotation!==0?4:3)))+'px;width:'+(width-offset+2-(border*2))+'px;height:'+(border*3)+'px;"></v:rect>';
 				text = get_widthText(text,ww,border*1.5,100,100); tw = get_textWidth(text,border*1.5,100,100); 
-				txt = get_strokeText(text,((width-offset)-tw)/2,height-offset-(border*(document.documentMode==8&&rotation!=0?3.4:2.4)),border*1.5,100,100,100,"sans-serif",tcolor,1,0);
+				txt = get_strokeText(text,((width-offset)-tw)/2,height-offset-(border*(document.documentMode==8&&rotation!==0?3.4:2.4)),border*1.5,100,100,100,"sans-serif",tcolor,1,0);
 				shade = '<v:rect strokeweight="0" filled="'+f+'" stroked="f" fillcolor="transparent" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:'+db+'px;left:'+db+'px;width:'+(width-offset-(2*border))+'px;height:'+hz+'px;"><v:fill method="sigma" type="gradient" angle="0" color="#000000" opacity="0" color2="#000000" o:opacity2="'+(opacity/2)+'" /></v:rect><v:rect strokeweight="0" filled="'+f+'" stroked="f" fillcolor="transparent" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:'+(height-offset-(border*3)-hz)+'px;left:'+border+'px;width:'+(width-offset-(2*border))+'px;height:'+hz+'px;"><v:fill method="sigma" type="gradient" angle="0" color="#ffffff" opacity="'+(opacity*0.75)+'" color2="#ffffff" o:opacity2="0" /></v:rect><v:rect strokeweight="2" filled="f" stroked="t" strokecolor="'+color+'" fillcolor="transparent" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:'+border+'px;left:'+border+'px;width:'+(width-offset-(2*border))+'px;height:'+(height-offset-(4*border))+'px;"><v:fill color="#ffffff" opacity="0" /></v:rect>';
 			}else {
 				shade = '<v:rect strokeweight="0" filled="'+f+'" stroked="f" fillcolor="transparent" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:'+db+'px;left:'+db+'px;width:'+(width-offset-(2*border))+'px;height:'+hz+'px;"><v:fill method="sigma" type="gradient" angle="0" color="#000000" opacity="0" color2="#000000" o:opacity2="'+(opacity/2)+'" /></v:rect><v:rect strokeweight="0" filled="'+f+'" stroked="f" fillcolor="transparent" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:'+(height-offset-border-hz)+'px;left:'+border+'px;width:'+(width-offset-(2*border))+'px;height:'+hz+'px;"><v:fill method="sigma" type="gradient" angle="0" color="#ffffff" opacity="'+(opacity*0.75)+'" color2="#ffffff" o:opacity2="0" /></v:rect><v:rect strokeweight="2" filled="f" stroked="t" strokecolor="'+color+'" fillcolor="transparent" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:'+border+'px;left:'+border+'px;width:'+(width-offset-(2*border))+'px;height:'+(height-offset-(2*border))+'px;"><v:fill color="#ffffff" opacity="0" /></v:rect>';
 			}
-			if(preserve==0) {
+			if(preserve===0) {
 				fill = '<v:image src="'+image.src+'" style="zoom:1;margin:0px;padding:0px;display:block;position:absolute;top:'+db+'px;left:'+db+'px;width:'+(width-offset-(2*border))+'px;height:'+(height-offset-(2*border))+'px;"></v:image>';
 			}else {
 				if(width>height) {
@@ -284,9 +284,9 @@ function addIEInstant() {
 			vml.style.visibility = 'visible';
 			vml.src = image.src; vml.alt = image.alt;
 			vml.width = image.width; vml.height = image.height;
-			if(image.id!='') vml.id = image.id;
-			if(image.title!='') vml.title = image.title;
-			if(image.getAttribute('onclick')!='') vml.setAttribute('onclick',image.getAttribute('onclick'));
+			if(image.id!=='') vml.id = image.id;
+			if(image.title!=='') vml.title = image.title;
+			if(image.getAttribute('onclick')!=='') vml.setAttribute('onclick',image.getAttribute('onclick'));
 			object.replaceChild(vml,image);
 		}
 	}
@@ -314,22 +314,22 @@ function addInstant() {
 			ishadow = getClassValue(classes,"ishadow");
 			if(ishadow>0) opacity=ishadow/100;
 			icolor = getClassColor(classes,"icolor");
-			if(icolor!=0) color = icolor;
+			if(icolor!==0) color = icolor;
 			itiltleft = getClassAttribute(classes,"itiltleft");
 			itiltright = getClassAttribute(classes,"itiltright");
 			itiltnone = getClassAttribute(classes,"itiltnone");
 			itxtcol = getClassColor(classes,"itxtcol");
-			if(itxtcol!=0) tcolor = itxtcol;
+			if(itxtcol!==0) tcolor = itxtcol;
 			itxttitle = getClassAttribute(classes,"itxttitle");
 			itxtalt = getClassAttribute(classes,"itxtalt");
 			historical = getClassAttribute(classes,"historical");
 			noshading = getClassAttribute(classes,"noshading");
 			nocorner = getClassAttribute(classes,"nocorner");
 			preserve = getClassAttribute(classes,"preserve");
-			if(historical==true) nocorner = false;
-			if(itiltright==true) tilt = 'r';
-			if(itiltnone==true) tilt = 'n';
-			if(itiltleft==true) tilt = 'l';
+			if(historical===true) nocorner = false;
+			if(itiltright===true) tilt = 'r';
+			if(itiltnone===true) tilt = 'n';
+			if(itiltleft===true) tilt = 'l';
 			newClasses = getClasses(classes,"instant");
 			canvas.className = newClasses;
 			canvas.style.cssText = image.style.cssText;
@@ -338,10 +338,10 @@ function addInstant() {
 			canvas.height = image.height;
 			canvas.width = image.width;
 			canvas.src = image.src; canvas.alt = image.alt;
-			if(image.id!='') canvas.id = image.id;
-			if(image.title!='') canvas.title = image.title;
-			if(image.getAttribute('onclick')!='') canvas.setAttribute('onclick',image.getAttribute('onclick'));
-			text = canvas.alt!=''&&itxtalt!=0?canvas.alt:canvas.title!=''&&itxttitle!=0?canvas.title:'';
+			if(image.id!=='') canvas.id = image.id;
+			if(image.title!=='') canvas.title = image.title;
+			if(image.getAttribute('onclick')!=='') canvas.setAttribute('onclick',image.getAttribute('onclick'));
+			text = canvas.alt!==''&&itxtalt!==0?canvas.alt:canvas.title!==''&&itxttitle!==0?canvas.title:'';
 			border = Math.round(((canvas.width+canvas.height)/2)*0.05); db = Math.round(Math.max(canvas.width,canvas.height)*0.05); 
 			offset = border/2; ww=canvas.width-(border*2); hh=canvas.height-(border*2);
 			inset = Math.floor(Math.min(Math.max(border/8,1),2));
@@ -374,7 +374,7 @@ function addInstant() {
 			context.fillStyle = 'rgba(0,0,0,'+opacity+')';
 			context.fillRect(border,border,canvas.width-(border*2),canvas.height-(border*2));
 			if(!window.opera) addLining(context,1.5,1.5,canvas.width-3,canvas.height-3,opacity,inset,false,color);
-			if(preserve==0) {
+			if(preserve===0) {
 				context.drawImage(image,border,border,canvas.width-(border*2),canvas.height-(border*2));
 			}else {
 				if(canvas.width>canvas.height) {
@@ -392,18 +392,19 @@ function addInstant() {
 				context.drawImage(image,border-xo,border-yo,ww,hh);
 				context.restore();
 			}
-			if(typeof set_textRenderContext=='function' && text!='') {				set_textRenderContext(context);
+			if(typeof set_textRenderContext=='function' && text!=='') {
+				set_textRenderContext(context);
 				if(check_textRenderContext(context)) {
 					context.save();
 					context.beginPath(); context.rect(1,canvas.height-(border*3),canvas.width-2,(border*3)); context.closePath(); context.fillStyle = color; context.fill();
 					context.restore(); 
-					if(noshading==0) addShading(context,border,border,canvas.width-(border*2),canvas.height-(border*4),opacity);
+					if(noshading===0) addShading(context,border,border,canvas.width-(border*2),canvas.height-(border*4),opacity);
 					if(!window.opera) addLining(context,border,border,canvas.width-(border*2),canvas.height-(border*4),opacity,inset,true);
 					context.strokeStyle = tcolor; text=get_widthText(text,ww,border*1.5,100,100); tw=get_textWidth(text,border*1.5,100,100);
 					context.strokeText(text,border+((ww-tw)/2),canvas.height-(border*2.4),border*1.5,100,100,100);
 				}
 			}else {
-				if(noshading==0) addShading(context,border,border,canvas.width-(border*2),canvas.height-(border*2),opacity);
+				if(noshading===0) addShading(context,border,border,canvas.width-(border*2),canvas.height-(border*2),opacity);
 				if(!window.opera) addLining(context,border,border,canvas.width-(border*2),canvas.height-(border*2),opacity,inset,true);
 			}
 			context.restore();
@@ -412,5 +413,5 @@ function addInstant() {
 	}
 }
 
-var instantOnload = window.onload;
-window.onload = function () { if(instantOnload) instantOnload(); if(isIE){addIEInstant(); }else {addInstant();}}
+if(window.addEventListener) window.addEventListener("load",addInstant,false);
+else window.attachEvent("onload",addIEInstant);
