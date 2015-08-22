1f3fa7160d5c6ad6af467cf481a4e706a737246d
Kniebremser <kniebremser@fluchtsportler.de>
2015-08-22 09:20:36 +0200

    changed widget google translator WP code conform
    -add missed docblocks
    - changed of functions in public functions

--- widgets/translator.php
+++ widgets/translator.php
@@ -6,9 +6,11 @@ Description:
 Author: Philip M. Hofer (Frumph)
 Author URI: http://frumph.net/
 Version: 1.3
-
 */
 
+/**
+ * Adds Google Translator widget.
+ */
 class comicpress_google_translate_widget extends WP_Widget {
 
 	/**
@@ -18,13 +20,21 @@ class comicpress_google_translate_widget extends WP_Widget {
 		parent::__construct(
 			__CLASS__, // Base ID
 			__( 'ComicPress - Google Translator', 'comicpress' ), // Name
-			array( 'classname' => __CLASS__, 'description' => __( 'Translate your site with Google.', 'comicpress' ), )
+			array( 'classname' => __CLASS__, 'description' => __( 'Translate your site with Google.', 'comicpress' ), ) //Args
 		);
 	}
 	
-	function widget($args, $instance) {
+/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget($args, $instance) {
 		global $post;
-		extract($args, EXTR_SKIP); 
+		extract($args, EXTR_SKIP);
 		echo $before_widget;
 		$title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']); 
 		if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }; ?>
@@ -35,13 +45,30 @@ class comicpress_google_translate_widget extends WP_Widget {
 		echo $after_widget;
 	}
 	
-	function update($new_instance, $old_instance) {
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update($new_instance, $old_instance) {
 		$instance = $old_instance;
 		$instance['title'] = strip_tags($new_instance['title']);
 		return $instance;
 	}
 	
-	function form($instance) {
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form($instance) {
 		$instance = wp_parse_args( (array) $instance, array( 'title' => '') );
 		$title = strip_tags($instance['title']);
 		?>
@@ -50,6 +77,7 @@ class comicpress_google_translate_widget extends WP_Widget {
 	}
 }
 
+// register Google Translator widget
 add_action( 'widgets_init', function(){
 	register_widget('comicpress_google_translate_widget');
 });
