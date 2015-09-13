af1cd5006a4d71dfede4a242cb116e3ab7b8dd64
Philip M. Hofer <philip@frumph.net>
2015-09-13 02:05:42 -0700

    HTML5 footer compliancy with <footer></footer> and it's now free floating out of the #page constraints - need to adjust CSS

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
-		<?php get_template_part('layout', 'foot'); ?>
-		<div id="footer">
-			<?php do_action('comicpress-footer'); ?>
-			<div id="footer-sidebar-wrapper">
-			<?php 
-				comicpress_get_sidebar('footer-left');
-				comicpress_get_sidebar('footer');
-				comicpress_get_sidebar('footer-right');
-			?>
-			</div>
-			<div class="clear"></div>
-		</div>	
 	</div> <!-- // #page -->
+</div> <!-- / #page-wrap -->
+<?php get_template_part('layout', 'foot'); ?>
+<footer id="footer">
+	<?php do_action('comicpress-footer'); ?>
+	<div id="footer-sidebar-wrapper">
+	<?php 
+		comicpress_get_sidebar('footer-left');
+		comicpress_get_sidebar('footer');
+		comicpress_get_sidebar('footer-right');
+	?>
+	</div>
+	<div class="clear"></div>
 	<div id="footer-menubar-wrapper">
 		<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'depth' => 1, 'fallback_cb' => false, 'container_class' => 'footmenu', 'theme_location' => 'Footer' ) ); ?>
 		<div class="clear"></div>
@@ -18,8 +18,8 @@
 	<?php if (!comicpress_themeinfo('disable_footer_text')) comicpress_copyright_text(); ?>
 	<?php if (comicpress_themeinfo('enable_debug_footer_code')) { ?>
 		<p><?php echo get_num_queries() ?> queries. <?php if (function_exists('memory_get_usage')) { $unit=array('b','kb','mb','gb','tb','pb'); echo @round(memory_get_usage(true)/pow(1024,($i=floor(log(memory_get_usage(true),1024)))),2).' '.$unit[$i]; ?> Memory usage. <?php } timer_stop(1) ?> seconds.</p>
-	<?php } ?>	
-</div> <!-- / #page-wrap -->
+	<?php } ?>		
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
