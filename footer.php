b68607f2c3a8ae77b823f20061408a899ba4e882
Kniebremser <kniebremser@fluchtsportler.de>
2015-09-13 17:56:11 +0200

    removed unused whitespace

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 	<?php if (!comicpress_themeinfo('disable_footer_text')) comicpress_copyright_text(); ?>
 	<?php if (comicpress_themeinfo('enable_debug_footer_code')) { ?>
 		<p><?php echo get_num_queries() ?> queries. <?php if (function_exists('memory_get_usage')) { $unit=array('b','kb','mb','gb','tb','pb'); echo @round(memory_get_usage(true)/pow(1024,($i=floor(log(memory_get_usage(true),1024)))),2).' '.$unit[$i]; ?> Memory usage. <?php } timer_stop(1) ?> seconds.</p>
-	<?php } ?>		
+	<?php } ?>
 </footer>
 <?php wp_footer(); ?>
 </body>
