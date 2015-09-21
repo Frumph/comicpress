f609da0c3a6c15fc31b8533242c2c0f758435ed9
Kniebremser <kniebremser@fluchtsportler.de>
2015-09-21 11:58:10 +0200

    fixed a error from me in the user info table
    end of } heard outside the Tables row

--- author.php
+++ author.php
@@ -50,7 +50,7 @@ if(get_query_var('author_name') ) {
 						</tr>
 						<tr>
 						<td class="user-info-name"><?php _e( 'Posts #', 'comicpress' ); ?></td>
-						<td class="user-info-value"><?php $author_id= get_the_author_meta('ID'); echo count_user_posts( $author_id ); ?></td>
+						<td class="user-info-value"><?php $author_id = get_the_author_meta('ID'); echo count_user_posts( $author_id ); ?></td>
 						</tr>
 					</table>
 					<?php } ?>
@@ -58,30 +58,34 @@ if(get_query_var('author_name') ) {
 					<table class="user-contacts">
 					<?php if (current_user_can('manage_options')) { ?>
 						<tr>
-							<td class="user-contacts-serv"><?php _e('Email','comicpress'); ?></td>
+							<td class="user-contacts-serv"><?php _e( 'Email', 'comicpress' ); ?></td>
 							<td class="user-contacts-url"><a href="mailto://<?php echo $curauth->user_email; ?>" target="_blank"><?php echo $curauth->user_email; ?></a></td>
 						</tr>
 					<?php } ?>
 					<?php if (!empty($curauth->user_url)) { ?>
 						<tr>
-							<td class="user-contacts-serv"><?php _e('Website','comicpress'); ?></td>
-							<td class="user-contacts-url"><a href="<?php echo $curauth->user_url; ?>" target="_blank"><?php echo $curauth->user_url; ?></a><?php } ?></td>
-						</tr>	
-					<?php if (!empty($curauth->twitter)) { ?>
-						<tr>
-							<td class="user-contacts-serv"><?php _e('Twitter','comicpress'); ?></td>
-							<td class="user-contacts-url"><a href="<?php echo $curauth->twitter; ?>" target="_blank"><?php echo $curauth->twitter; ?></a><?php } ?></td>
+							<td class="user-contacts-serv"><?php _e( 'Website', 'comicpress' ); ?></td>
+							<td class="user-contacts-url"><a href="<?php echo $curauth->user_url; ?>" target="_blank"><?php echo $curauth->user_url; ?></a></td>
 						</tr>
-					<?php if (!empty($curauth->facebook)) { ?>
+					<?php }
+						if (!empty($curauth->twitter)) { ?>
 						<tr>
-							<td class="user-contacts-serv"><?php _e('Facebook','comicpress'); ?></td>
-							<td class="user-contacts-url"><a href="<?php echo $curauth->facebook; ?>" target="_blank"><?php echo $curauth->facebook; ?></a><?php } ?></td>
+							<td class="user-contacts-serv"><?php _e( 'Twitter', 'comicpress' ); ?></td>
+							<td class="user-contacts-url"><a href="<?php echo $curauth->twitter; ?>" target="_blank"><?php echo $curauth->twitter; ?></a></td>
 						</tr>
-					<?php if (!empty($curauth->googleplus)) { ?>
+					<?php }
+						if (!empty($curauth->facebook)) { ?>
 						<tr>
-							<td class="user-contacts-serv"><?php _e('Google+','comicpress'); ?></td>
-							<td class="user-contacts-url"><a href="<?php echo $curauth->googleplus; ?>" target="_blank" rel="me"><?php echo $curauth->googleplus; ?></a><?php } ?></td>
+							<td class="user-contacts-serv"><?php _e( 'Facebook', 'comicpress' ); ?></td>
+							<td class="user-contacts-url"><a href="<?php echo $curauth->facebook; ?>" target="_blank"><?php echo $curauth->facebook; ?></a></td>
 						</tr>
+					<?php }
+						if (!empty($curauth->googleplus)) { ?>
+						<tr>
+							<td class="user-contacts-serv"><?php _e( 'Google+', 'comicpress' ); ?></td>
+							<td class="user-contacts-url"><a href="<?php echo $curauth->googleplus; ?>" target="_blank" rel="me"><?php echo $curauth->googleplus; ?></a></td>
+						</tr>
+					<?php } ?>
 					</table>
 				</div>
 				<?php if (!empty($curauth->description)) { ?>
