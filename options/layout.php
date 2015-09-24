de01573f9ff0ee94530166426131a244f61b7c7f
Kniebremser <kniebremser@fluchtsportler.de>
2015-09-24 10:21:19 +0200

    a little more width for layout select
    so even translated the entire content is visible

--- options/layout.php
+++ options/layout.php
@@ -27,7 +27,7 @@
                     $comicpress_options['layout'] = '3c';
 				?>
 				<tr class="alternate">
-					<th scope="row" style="width:250px"><label for="layout" style="text-align:left"><?php _e( 'Choose Your Website Layout', 'comicpress' ); ?></label>
+					<th scope="row" style="width:280px"><label for="layout" style="text-align:left"><?php _e( 'Choose Your Website Layout', 'comicpress' ); ?></label>
 						<select name="layout" id="layout" onchange="lshowimage(this,'thelayout')">
 							<option class="level-0" value="3c" <?php if ($comicpress_options['layout'] == '3c') { ?>selected="selected" <?php } ?>><?php _e( '3 Column &mdash; Standard [default]', 'comicpress' ); ?></option>
 														
