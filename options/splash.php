f502f34622499e6f46ddfe376a90377a9d1936bb
Philip M. Hofer <philip@frumph.net>
2015-09-27 15:57:10 -0700

    technical support information added to splash page

--- options/splash.php
+++ options/splash.php
@@ -1,8 +1,29 @@
 <div id="comicpress-splash">
 	<?php wp_nonce_field('update-options') ?>
 		<div class="comicpress-options">
+			<table class="widefat">
+				<thead>
+					<tr>
+						<th colspan="3"><?php _e('Technical Support','comiceasel'); ?></th>
+					</tr>
+				</thead>
+				<tr>
+					<td>
+						<?php _e('You can contact me for technical support at any of the following locations: ','comiceasel'); ?><br />
+						<em><?php _e('(best chances to reach me at the top going down to the least chance)','comiceasel'); ?></em><br />
+						<br />
+						<?php _e('Form Email:','comiceasel'); ?> <a href="http://frumph.net/contact/">http://frumph.net/contact/</a><br />
+						<?php _e('Via Facebook:','comiceasel'); ?> <a href="http://facebook.com/philip.hofer">http://facebook.com/philip.hofer</a><br />
+						<?php _e('WebComics.COM Forums:', 'comiceasel'); ?> <a href="http://webcomics.com/forum/website-design-assistance/">http://webcomics.com/forum/website-design-assistance/</a>  <?php _e('(Subscription only)','comiceasel'); ?><br />
+						<?php _e('WordPress.ORG Forums:', 'comiceasel'); ?> <a href="https://wordpress.org/support/theme/comicpress">https://wordpress.org/support/theme/comicpress</a> <?php _e('(rarely go here)','comiceasel'); ?><br />
+						<?php _e('On Twitter:', 'comiceasel'); ?> <a href="http://twitter.com/Frumph">http://twitter.com/Frumph</a> <?php _e('(Twitter and I are not friends atm)','comiceasel'); ?> <br />
+						<br />
+						<?php _e('Please take note that I speak American-English only, pardon my ignorance and will try to understand you as best I can.', 'comicpress'); ?><br />
+					</td>
+				</tr>
+			</table>
+			<br />		
 			<strong><?php _e( 'Schemes, layout and Site Width along with other customizations can be modified in the appearance &#10132; customize section of the wp-admin.', 'comicpress' ); ?></strong><br />
-			TODO: Add introduction about the theme here and some cool graphic image and maybe some credits.<br />
 			<br />
 		</div>
 </div>
