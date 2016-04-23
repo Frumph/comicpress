0e2d83f69da5b50e500fc04ba5a211146ba4c7e2
Philip M. Hofer <philip@frumph.net>
2016-04-23 11:07:18 -0700

    fixed pagination in blog.php template

--- blog.php
+++ blog.php
@@ -4,19 +4,25 @@ Template Name: Blog
 */
 get_header();
 
+$paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
+
 $blog_query = array(
-		'paged' => get_query_var('paged'),
+		'paged' => $paged,
 		'post_type' => 'post'
 		);
 		
-$blogpage = new WP_Query(); $blogpage->query($blog_query);
-if ($blogpage->have_posts()) {
-	while ($blogpage->have_posts()) : $blogpage->the_post();
+$wp_query = new WP_Query(); $wp_query->query($blog_query);
+
+if (have_posts()) {
+	while (have_posts()) : the_post();
 		$withcomment = 0;
 		get_template_part('content', get_post_format());
 	endwhile;
 	comicpress_pagination();
 }
+
+wp_reset_postdata();
+wp_reset_query();
 
 get_footer();
 
