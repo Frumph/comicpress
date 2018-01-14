dd937f53e7e9e66f670420eb20b3910ff8f9a28b
Frumph <philip@frumph.net>
2018-01-13 20:08:18 -0800

    Re-init of home-splash location the system ignored
    Signed-off-by: Frumph <philip@frumph.net>

--- layout-head.php
+++ layout-head.php
@@ -6,23 +6,26 @@
 	<div id="subcontent-wrapper">
 <?php
 if (!comicpress_is_signup() && !comicpress_sidebars_disabled()) {
-		if (comicpress_is_layout('2cl,3c,3cl,3clgn')) comicpress_get_sidebar('left');
-		if (comicpress_is_layout('3cl')) comicpress_get_sidebar('right');
+	if (comicpress_is_layout('2cl,3c,3cl,3clgn')) comicpress_get_sidebar('left');
+	if (comicpress_is_layout('3cl')) comicpress_get_sidebar('right');
+}
+if (is_front_page() && !comicpress_sidebars_disabled()) {
+	comicpress_get_sidebar('home-splash');
 }
 ?>
 		<div id="content-column">
-			<?php 
-				if (get_theme_mod('comicpress-customize-comic-in-column', false)) do_action('comic-area');
-				if (!comicpress_is_signup() && !comicpress_sidebars_disabled()) {
-					if (comicpress_is_layout('3crgn')) comicpress_get_sidebar('left');
-				}
+<?php 
+if (get_theme_mod('comicpress-customize-comic-in-column', false)) do_action('comic-area');
+if (!comicpress_is_signup() && !comicpress_sidebars_disabled()) {
+	if (comicpress_is_layout('3crgn')) comicpress_get_sidebar('left');
+}
 			?>
-			<div id="content" class="narrowcolumn">
+			<div id="content" class="narrowcolumn">		
 				<?php do_action('comicpress-narrowcolumn-area'); ?>
-				<?php
-					if (!comicpress_sidebars_disabled()) {
-						if (!is_front_page() && !comicpress_themeinfo('over-blog-sidebar-all-posts')) return;
-							comicpress_get_sidebar('over-blog');
-					}
+<?php
+if (!comicpress_sidebars_disabled()) {
+	if (!is_front_page() && !comicpress_themeinfo('over-blog-sidebar-all-posts')) return;
+	comicpress_get_sidebar('over-blog');
+}
 				?>
 				<?php do_action('comic-blog-area'); ?>
