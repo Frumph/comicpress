c1ab075572b1aa7a743ff33101fcc55aecacc251
Kniebremser <kniebremser@fluchtsportler.de>
2019-06-08 13:36:36 +0200

    reduce wp Coding standards messages part 1

--- 404.php
+++ 404.php
@@ -1,13 +1,30 @@
-<?php get_header(); ?>
+<?php
+/**
+ * 404 Page
+ * by Philip M. Hofer (Frumph)
+ * http://frumph.net/
+ *
+ * Content for the 404 page.
+ *
+ * @package Comicpress
+ */
+
+get_header(); ?>
 
 <div class="post uentry type-page">
 	<div class="post-content">
 		<div class="post-info">
-			<h2 class="page-title"><?php _e( 'Page Not Found', 'comicpress' ); ?></h2>
+			<h2 class="page-title">
+				<?php _e( 'Page Not Found', 'comicpress' ); ?>
+			</h2>
 		</div>
 		<div class="entry">
-			<p><a href="<?php echo site_url(); ?>"><?php _e( 'Click here to return to the home page', 'comicpress' ); ?></a> <?php _e( 'or try a search:', 'comicpress' ); ?></p>
-			<p><?php get_search_form(); ?></p>
+			<p>
+			<a href="<?php echo site_url(); ?>"><?php _e( 'Click here to return to the home page', 'comicpress' ); ?></a> <?php _e( 'or try a search:', 'comicpress' ); ?>
+			</p>
+			<p>
+			<?php get_search_form(); ?>
+			</p>
 			<div class="clear"></div>
 		</div>
 		<div class="clear"></div>
--- author.php
+++ author.php
@@ -9,16 +9,18 @@
  * The table 'user-info' is only visible to admins.
  * For table 'user-contacts' can you find possible contact methods
  * in profile-options.php function add_new_contactmethods.
+ *
+ * @package Comicpress
  */
 
 remove_action( 'init', 'the_neverending_home_page_init', 10 );
 get_header();
 
-if(get_query_var('author_name') ) {
-	$curauth = get_user_by('slug', get_query_var('author_name'));
+if ( get_query_var( 'author_name' ) ) {
+	$curauth = get_user_by( 'slug', get_query_var( 'author_name' ) );
 } else {
-	$curauth = get_userdata(get_query_var('author'));
-} if (empty($curauth)) { ?>
+	$curauth = get_userdata(get_query_var( 'author' ) );
+} if ( empty( $curauth ) ) { ?>
 
 <h2><?php _e( 'No such author.', 'comicpress' ); ?></h2>
 <?php } else { ?>
@@ -27,29 +29,28 @@ if(get_query_var('author_name') ) {
 	<div class="post-content">
 		<div class="entry">
 			<div class="userpage-avatar">
-				<?php echo str_replace('photo', 'photo instant nocorner itxtalt', get_avatar($curauth->user_email, 64, comicpress_random_default_avatar($curauth->user_email), esc_attr($curauth->display_name, 1))); ?>
+				<?php echo str_replace( 'photo', 'photo instant nocorner itxtalt', get_avatar( $curauth->user_email, 64, comicpress_random_default_avatar( $curauth->user_email ), esc_attr( $curauth->display_name, 1) ) ); ?>
 			</div>
 			<div class="userpage-info">
 				<div class="userpage-bio">
 					<?php
-					if($curauth->display_name)
-						$authorname = $curauth->display_name;
-					elseif($curauth->user_nickname)
-						$authorname = $curauth->nickname;
-					elseif($curauth->user_nicename)
-						$authorname = $curauth->user_nicename;
-					else
-						$authorname = $curauth->user_login;
+					if ( $curauth->display_name )
+						$authorname  = $curauth->display_name;
+					elseif ( $curauth->user_nickname )
+						$authorname  = $curauth->nickname;
+					elseif ( $curauth->user_nicename )
+						$authorname  = $curauth->user_nicename;
+					else $authorname = $curauth->user_login;
 					?>
 					<h2><?php echo $authorname; ?></h2>
-					<?php if (current_user_can('manage_options')) { ?>
+					<?php if ( current_user_can( 'manage_options' ) ) { ?>
 					<table class="user-info">
 						<tr>
 							<td class="user-info-name">
 								<?php _e( 'Registered on', 'comicpress' ); ?>
 							</td>
 							<td class="user-info-value">
-								<?php echo date_i18n(get_option('date_format'), strtotime($curauth->user_registered)); ?>
+								<?php echo date_i18n( get_option( 'date_format' ), strtotime( $curauth->user_registered ) ); ?>
 							</td>
 						</tr>
 						<tr>
@@ -57,14 +58,17 @@ if(get_query_var('author_name') ) {
 							<?php _e( 'Posts #', 'comicpress' ); ?>
 						</td>
 						<td class="user-info-value">
-							<?php $author_id = get_the_author_meta('ID'); echo count_user_posts( $author_id ); ?>
+							<?php
+							$author_id = get_the_author_meta( 'ID' );
+							echo count_user_posts( $author_id );
+							?>
 						</td>
 						</tr>
 					</table>
 					<?php } ?>
 					<br />
 					<table class="user-contacts">
-						<?php if (current_user_can('manage_options')) { ?>
+						<?php if ( current_user_can( 'manage_options' ) ) { ?>
 						<tr>
 							<td class="user-contacts-serv">
 								<?php _e( 'Email', 'comicpress' ); ?>
@@ -73,8 +77,10 @@ if(get_query_var('author_name') ) {
 								<a href="mailto://<?php echo $curauth->user_email; ?>" target="_blank"><?php echo $curauth->user_email; ?></a>
 							</td>
 						</tr>
-						<?php }
-						if (!empty($curauth->user_url)) { ?>
+							<?php
+						}
+						if ( !empty( $curauth->user_url ) ) {
+							?>
 						<tr>
 							<td class="user-contacts-serv">
 								<?php _e( 'Website', 'comicpress' ); ?>
@@ -83,8 +89,10 @@ if(get_query_var('author_name') ) {
 								<a href="<?php echo $curauth->user_url; ?>" target="_blank"><?php echo $curauth->user_url; ?></a>
 							</td>
 						</tr>
-						<?php }
-						if (!empty($curauth->twitter)) { ?>
+							<?php
+						}
+						if ( ! empty( $curauth->twitter ) ) {
+							?>
 						<tr>
 							<td class="user-contacts-serv">
 								<?php _e( 'Twitter', 'comicpress' ); ?>
@@ -93,8 +101,10 @@ if(get_query_var('author_name') ) {
 								<a href="<?php echo $curauth->twitter; ?>" target="_blank"><?php echo $curauth->twitter; ?></a>
 							</td>
 						</tr>
-						<?php }
-						if (!empty($curauth->facebook)) { ?>
+							<?php
+						}
+						if ( ! empty( $curauth->facebook ) ) {
+							?>
 						<tr>
 							<td class="user-contacts-serv">
 								<?php _e( 'Facebook', 'comicpress' ); ?>
@@ -107,7 +117,7 @@ if(get_query_var('author_name') ) {
 					</table>
 				</div>
 
-				<?php if (!empty($curauth->description)) { ?>
+				<?php if ( ! empty( $curauth->description ) ) { ?>
 				<div class="userpage-desc">
 					<?php echo $curauth->description; ?>
 				</div>
@@ -116,18 +126,20 @@ if(get_query_var('author_name') ) {
 			</div>
 			<div class="clear"></div>
 
-			<?php if (have_posts()) { ?>
+			<?php if ( have_posts() ) { ?>
 			<div class="userpage-posts">
 				<h3><?php _e( 'Posts by', 'comicpress' ); ?> <?php echo $authorname; ?> &not;</h3>
 				<br />
 				<table class="author-posts">
-					<?php while (have_posts()) : the_post(); ?>
+					<?php
+					while ( have_posts() ) : the_post();
+						?>
 					<tr>
 						<td class="author-archive-date" align="right">
-							<?php the_time('M j, Y') ?>
+							<?php the_time( 'M j, Y' ); ?>
 						</td>
 						<td class="author-archive-title">
-							<a href="<?php the_permalink(); ?>"><?php the_title() ?></a>
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 						</td>
 					</tr>
 					<?php endwhile; ?>
@@ -138,5 +150,6 @@ if(get_query_var('author_name') ) {
 	</div>
 	<div class="post-foot"></div>
 </div>
-<?php }
+	<?php
+}
 get_footer();
