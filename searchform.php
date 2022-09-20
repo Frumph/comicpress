a574bdc38cd4314df6c923dc5db2586708f1fbd0
Frumph <philip@frumph.net>
2022-09-20 00:32:00 -0700

    using esc_attr_e

--- searchform.php
+++ searchform.php
@@ -12,12 +12,12 @@
 ?>
 
 <form method="get" class="searchform" action="<?php echo esc_html( home_url() ); ?>">
-	<input type="text" value="<?php esc_html_e( 'Search...', 'comicpress' ); ?>" name="s" class="s-search" onfocus="this.value=(this.value=='<?php esc_html_e( 'Search...', 'comicpress' ); ?>') ? '' : this.value;" onblur="this.value=(this.value=='') ? '<?php esc_html_e( 'Search...', 'comicpress' ); ?>' : this.value;" />
+	<input type="text" value="<?php esc_attr_e( 'Search...', 'comicpress' ); ?>" name="s" class="s-search" onfocus="this.value=(this.value=='<?php esc_attr_e( 'Search...', 'comicpress' ); ?>') ? '' : this.value;" onblur="this.value=(this.value=='') ? '<?php esc_attr_e( 'Search...', 'comicpress' ); ?>' : this.value;" />
 	<button type="submit">
 
 		<?php
 		/* translators: Content for search button */
-		esc_html_e( '&raquo;', 'comicpress' );
+		esc_attr_e( '&raquo;', 'comicpress' );
 		?>
 
 	</button>
